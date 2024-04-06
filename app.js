4ecd34b972080da468704b4c9fb8a992389dda31
ki3mi <ccentemejiajosecarlos@gmail.com>
2024-04-06 12:51:52 -0500

    listo

--- app.js
+++ app.js
@@ -8,9 +8,11 @@ import locationRoutes from './routes/Routes.js'
 
 const app = express()
 
-app.use( cors() )
-
+// app.use( cors())
+app.use( cors({origin:"*"}))
 app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
 app.use('/locations', locationRoutes)
 
 try {
